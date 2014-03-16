d1db13a099704d02ccf9ecd4d5980ca63886b93b
Zachary Priddy <me@zpriddy.com>
2014-03-16 00:25:28 -0700

    Carry-Skip adder added
    Added carry-skip n-bit adder

--- project-1-zach/full_adder.v
+++ project-1-zach/full_adder.v
@@ -18,6 +18,7 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+//FULL ADDER
 module full_adder(
 	// I/O Ports
 	output sum, c_out,
@@ -39,6 +40,7 @@ assign c_out = c_out;
 assign sum = sum;
 endmodule
 
+// RIPPLE ADDER
 module n_ripple_adder(sum,c_out,a,b,c_in);
 	parameter n = 4;
 	
@@ -63,3 +65,73 @@ module n_ripple_adder(sum,c_out,a,b,c_in);
 	end
 
 endmodule 
+
+// CARRY-SKIP ADDER
+module full_adder_p(sum,c_out,p,a,b,c_in);
+	input a,b,c_in;
+	output sum,c_out,p;
+	
+	assign{c_out,sum}=a+b+c_in;
+	assign p=a^b;
+endmodule 
+
+module n_carry_skip_adder(sum,c_out,a,b,c_in);
+	parameter n = 4; //How many sets of 4 bits
+	parameter bits = 4*n;
+	integer k;
+
+	
+	input [bits-1:0] a, b;
+	input c_in;
+	
+	output [bits-1:0] sum;
+	output c_out;
+	
+	reg c_out;
+	reg [bits-1:0] sum;
+	reg [bits:0] C, p;
+	reg [n:0] cs;
+	
+	reg Ct, sumt, pt, cst;
+	
+	always @(a or b or c_in) begin
+	C[0] = c_in;
+	cs[0] = 0;
+	for (k = 0 ; k < n ; k = k + 1) begin
+		#4 assign {Ct,sumt}=a[k*4]+b[k*4]+C[k*4];
+		assign pt=a[k*4]^b[k*4];
+		C[k*4+1] = Ct;
+		sum[k*4] = sumt;
+		p[k*4] = pt;
+		
+		
+		#4 assign {Ct,sumt}=a[k*4+1]+b[k*4+1]+C[k*4+1];
+		assign pt=a[k*4+1]^b[k*4+1];
+		C[k*4+2] = Ct;
+		sum[k*4+1] = sumt;
+		p[k*4+1] = pt;
+		
+		#4 assign {Ct,sumt}=a[k*4+2]+b[k*4+2]+C[k*4+2];
+		assign pt=a[k*4+2]^b[k*4+2];
+		C[k*4+3] = Ct;
+		sum[k*4+2] = sumt;
+		p[k*4+2] = pt;
+		
+		#4 assign {Ct,sumt}=a[k*4+3]+b[k*4+3]+C[k*4+3];
+		assign pt=a[k*4+3]^b[k*4+3];
+		C[k*4+4] = Ct;
+		sum[k*4+3] = sumt;
+		p[k*4+3] = pt;
+		
+		assign cst = C[k*4+4]|(p[k*4]&p[k*4+1]&p[k*4+2]&p[k*4+3]&cs[k*4]);
+		cs[k+1] = cst;
+		
+	end
+	
+	c_out = cs[n];
+	
+	end
+	
+endmodule
+	
+
